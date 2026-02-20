63ec8b859e0f02b95ce7157e5e0b6fbdc76ee6cd
meenakshi930 <meenakshi.gupta_cs.aiml24@gla.ac.in>
2026-02-20 09:54:39 +0530

    add configuration files for code formatting and migration state

--- src/main.cpp
+++ src/main.cpp
@@ -2,58 +2,13 @@
 /**
  * @file main.cpp
  * @brief Embedded Distance Measurement using Ultrasonic Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Meenakshi
+ * @date 2026-02-11
  *
  * @details
- * Measures distance using HC-SR04 ultrasonic sensor
- * and displays structured output via Serial Monitor.
+ * Measures distance using the HC-SR04 ultrasonic sensor
+ * and displays the calculated distance on the Serial Monitor.
  */
 
- // TODO 1:
- // Define TRIG pin (Use pin 9)
-
- // TODO 2:
- // Define ECHO pin (Use pin 10)
-
- // TODO 3:
- // Create variable to store duration
-
- // TODO 4:
- // Create variable to store calculated distance
-
-void setup() {
-
-    // TODO 5:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 6:
-    // Configure TRIG as OUTPUT
-
-    // TODO 7:
-    // Configure ECHO as INPUT
-
-    // TODO 8:
-    // Print system initialization message
-}
-
-void loop() {
-
-    // TODO 9:
-    // Set TRIG LOW for 2 microseconds
-
-    // TODO 10:
-    // Send 10 microsecond pulse on TRIG
-
-    // TODO 11:
-    // Measure pulse duration on ECHO using pulseIn()
-
-    // TODO 12:
-    // Calculate distance in cm
-
-    // TODO 13:
-    // Print calculated distance
-
-    // TODO 14:
-    // Add delay (500ms)
-}
+#define trigPin 9
+#define echoPin 10
